0143668abd484f0e47e139ad62ccf92edf46017d
brycches <brycches@byui.edu>
2023-09-20 11:15:34 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int gNumber = 0;
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 101);
+        do {
+            Console.Write("guess a number between 1 and 100. ");
+            string guessed = Console.ReadLine();
+            gNumber = int.Parse(guessed);
+
+            if (gNumber > mNumber )
+            {
+                Console.WriteLine("your guess is too high. ");
+            } else if (gNumber < mNumber)
+            {
+                Console.WriteLine("your guess is too low ");
+            } else
+            {
+                Console.WriteLine("you guessed correctly! ");
+                Console.WriteLine($"It took you {guesses} guesses")
+            }
+            int guesses += 1;
+        
+            } while (gNumber != mNumber);
+            
+        
+        
+            
+        
+
+
+
     }
 }
