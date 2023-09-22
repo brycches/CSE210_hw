e00322fac94e3f3687817796329b9de8a224c4dc
brycches <brycches@byui.edu>
2023-09-22 13:21:45 -0600

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int additional = ""
+        do{
+            Console.WriteLine("Please enter a number to add to the list, type 0 to quit ");
+            additional = Console.ReadLine();
+            if (additional != 0)
+            {
+                numbers.append(additional);
+            }
+
+        } while (additional != 0);
+        foreach (int number in numbers)
+            {
+                int total += number;
+            }
+            Console.WriteLine($"your numbers are {numbers} and thier total is {total}")
+        words.Add("phone");
+        words.Add("keyboard");
+        words.Add("mouse");
     }
 }
