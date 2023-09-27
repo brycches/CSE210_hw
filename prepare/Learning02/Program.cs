7a1d98fc183cb1249a868d73afdfc8d40a087ca2
brycches <brycches@byui.edu>
2023-09-27 14:52:45 -0600

    Commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job { Company = "Company A", JobTitle = "Title A", StartYear = 2010, EndYear = 2015 };
+
+
+        Job job2 = new Job { Company = "Company B", JobTitle = "Title B", StartYear = 2011, EndYear = 2016 };
+
+        // job1.Display();
+
+        // job2.Display();
+
+        Resume resume1 = new Resume { Name = "Bryce Chesley" };
+        resume1.Jobs.Add(job1);
+        resume1.Jobs.Add(job2);
+        resume1.Display();
     }
 }
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,22 @@
+public class Resume
+{
+    public string Name { get; set; }
+
+    public List<Job> Jobs { get; set; } = new List<Job>();
+
+    public Resume()
+    {
+
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (var job in Jobs)
+        {
+            job.Display();
+        }
+    }
+};
