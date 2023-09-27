fec946b191e7677ff16500f4e7534f896f41dd68
Bryce Chesley <brycches@gmail.com>
2023-09-27 14:00:42 -0600

    q

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,16 @@
+public class Job
+{
+    public string Company { get; set; }
+
+    public string JobTitle { get; set; }
+
+    public int StartYear { get; set; }
+
+    public int EndYear { get; set; }
+
+    public void Display()
+    {
+
+    }
+    
+}
